f40e35f9f3b0633bf06a4f570d436f135b8b93f3
Emile <emile.bettini@protonmail.ch>
2022-05-18 22:53:07 +0200

    update articles app

---
+++ articles/factories.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from random import randint
+
+from factory import SubFactory
+from factory.django import DjangoModelFactory
+
+from accounts.factories import UserFactory
+from references.factories import ReferenceFactory
+from articles.models import Article
+
+
+class ArticleFactory(DjangoModelFactory):
+    class Meta:
+        model = Article
+
+    audience = randint(1, 1000000)
+    price = Decimal(randint(1, 10000))
+    hardware = SubFactory(ReferenceFactory)
+    user = SubFactory(UserFactory)
--- articles/managers.py
+++ articles/managers.py
@@ -1,5 +1,16 @@
+from __future__ import annotations
+
+from decimal import Decimal
+from typing import Dict
+
 from django.db import models
 
 
 class ArticleManager(models.Manager):
-    ...
+    def get_catalog(self, user) -> Dict[str, Decimal | int]:
+        catalog = {"price": 0, "audience": 0}
+        for article in self.filter(user=user):
+            catalog["price"] += article.price
+            catalog["audience"] += article.audience
+        return catalog
+
