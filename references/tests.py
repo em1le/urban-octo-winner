bb69b11ec2de2781e3bd62886971b985d929f142
Emile <emile.bettini@protonmail.ch>
2022-05-18 22:55:05 +0200

    Update tests.py

--- references/tests.py
+++ references/tests.py
@@ -1,3 +1,22 @@
+
 from django.test import TestCase
 
-# Create your tests here.
+from references.models import Reference
+from articles.factories import ArticleFactory
+
+
+class HardwareTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.article = ArticleFactory()
+        super().setUpTestData()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+
+    def test_listing_by_model(self):
+        expected = {"audience": self.article.audience, "price": self.article.price}
+        reference_type = self.article.reference.kind
+        result = Reference.objects.listing_by_model(reference_type, self.article.user)
+        self.assertDictEqual(result, expected)
