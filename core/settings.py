9e34224dafe497726e97dd7825e942925c314d70
Emile <emile.bettini@protonmail.ch>
2022-05-19 21:51:42 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import sys
-
+import django_heroku
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -171,3 +171,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/dashboard"
 LOGIN_URL = "/accounts/login/"
 LOGOUT_REDIRECT_URL = "/"
+
+django_heroku.settings(locals())
